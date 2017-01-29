f3780180468978e44f369b8a0e672e3c0864281a
Peter de Beer <peter@inlinemedia.nl>
2017-01-29 14:11:40 +0100

    Add mdoels for player en teams

---
+++ src/Model/Table/PlayersTable.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Players Model
+ *
+ * @property \Cake\ORM\Association\HasMany $TeamsHasPlayers
+ *
+ * @method \App\Model\Entity\Player get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Player newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Player[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Player|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Player patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Player[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Player findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class PlayersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('players');
+        $this->displayField('name');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->hasMany('TeamsHasPlayers', [
+            'foreignKey' => 'player_id'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->integer('number')
+            ->requirePresence('number', 'create')
+            ->notEmpty('number');
+
+        return $validator;
+    }
+}
