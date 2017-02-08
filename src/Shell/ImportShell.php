2f38dceed9d9b8b28394a0f8c673c4562f0e4bc0
Peter de Beer <peter@inlinemedia.nl>
2017-02-08 16:24:34 +0100

    Add import for historc teams

--- src/Shell/ImportShell.php
+++ src/Shell/ImportShell.php
@@ -7,7 +7,10 @@ use Cake\Utility\Text;
 /**
  * Import shell command.
  *
+ *
  * @property \App\Model\Table\TeamsTable $Teams
+ * @property \App\Model\Table\YearsTable $Years
+ * @property \App\Model\Table\HistoryTeamsTable $HistoryTeams
  */
 class ImportShell extends Shell
 {
@@ -16,6 +19,8 @@ class ImportShell extends Shell
    {
        //Load Models
        $this->loadModel("Teams");
+       $this->loadModel("Years");
+       $this->loadModel("HistoryTeams");
 
        parent::__construct();
    }
@@ -51,6 +56,70 @@ class ImportShell extends Shell
             //Save team
             $this->Teams->save($teamEntities);
         }
+
+    }
+
+    public function addYears(){
+
+        for ($i = 1997; $i < 2016; $i++){
+            echo $i. " - ". ($i +1)."\n";
+
+            $year = $this->Years->newEntity();
+            $year->name = $i. " - ". ($i +1);
+            $year->start = $i;
+            $year->end = ($i + 1);
+            $year->sort_order = ($i - 1996);
+            $this->Years->save($year);
+
+
+        }
+    }
+
+    public function importTeams(){
+        //Get years
+
+        $years = $this->Years->find();
+
+        foreach ($years as $year){
+
+            //Get the comp
+
+            $url = "http://db.basketball.nl/db/json/team.pl?clb_ID=81&seizoen=".$year->start."-".$year->end;
+
+            $teams = file_get_contents($url);
+            $teams = json_decode($teams);
+
+            //Merge teams
+            $mTeams = [];
+            foreach ($teams->teams as $team){
+                if(!isset($mTeams[$team->id]['name'])){
+                    $mTeams[$team->id]['name'] = $team->naam;
+                    $mTeams[$team->id]['nbb'] = $team->id;
+                    $mTeams[$team->id]['comp_id_1'] = $team->comp_id;
+                    $mTeams[$team->id]['comp_id_2'] = null;
+                }else{
+                    $mTeams[$team->id]['comp_id_2'] = $team->comp_id;
+                }
+            }
+
+            //debug($mTeams);
+
+            //Save teams
+            foreach ($mTeams as $mTeam){
+                $hisTeam = $this->HistoryTeams->newEntity();
+                $hisTeam->name = $mTeam['name'];
+                $hisTeam->slug = strtolower(Text::slug(trim($mTeam['name'])));
+                $hisTeam->nbb_id = $mTeam['nbb'];
+                $hisTeam->comp_id_1 = $mTeam['comp_id_1'];
+                $hisTeam->comp_id_2 = $mTeam['comp_id_2'];
+                $hisTeam->year_id = $year->id;
+
+                $this->HistoryTeams->save($hisTeam);
+            }
+
+
+        }
+
 
     }
 }
